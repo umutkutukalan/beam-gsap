7e098e5ad55fc7582a24907be870d86cd97265f7
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-02-07 23:32:38 +0300

    added bottom-div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -59,6 +59,23 @@ const Home = () => {
         },
       }
     );
+    gsap.fromTo(
+      ".bottom-div",
+      { opacity: 1, y: 0, zIndex: 190, scale: 1 }, // Başlangıçta görünür
+      {
+        opacity: 0, // Kaybol
+        duration: 3, // 3 saniyede kaybol
+        scale: 0.5,
+        ease: "power1.inOut",
+        zIndex: 0,
+        scrollTrigger: {
+          trigger: ".box",
+          start: "top 20%",
+          end: "top 10%",
+          scrub: 1,
+        },
+      }
+    );
   }, []);
 
   return (
@@ -67,6 +84,15 @@ const Home = () => {
       <div className="box fixed left-1/2 transform -translate-x-1/2 bg-[#201bcb] rounded-lg w-full overflow-hidden">
         <div className="content z-200 relative ">
           <Container1 />
+        </div>
+      </div>
+      <div className="bottom-div fixed inset-0 flex items-center justify-center flex-col gap-10">
+        <h1 className="text-7xl text-white mb-4 mt-10">Meet the bright web</h1>
+        <div className="flex flex-col items-center gap-4">
+          <button className="bg-[#201bcb] text-white px-6 py-2 rounded-lg text-4xl px-10 py-5">
+            Download beam
+          </button>
+          <span className="text-sm text-gray-500">for macOS 11.3+</span>
         </div>
       </div>
     </div>
